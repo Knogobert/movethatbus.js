a61dbdcffa3640da114ee62006d46983de1aa4ff
Sven Parker <sven@svenparker.com>
2020-03-11 19:47:39 +0100

    Added starting blocks for canvas element

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
-const Hello = "World";
+function createCanvas() {
+    const canvas = document.createElement('canvas');
+    canvas.id = "movethatbus";
+    canvas.style.width = "100vh";
+    canvas.style.height = "100vh";
+    canvas.style.position = "absolute";
+    canvas.style.top = "0";
+    canvas.style.left = "0";
+    canvas.style.background = "rgba(200,55,77,.2)"; // !refactor
+    return canvas;
+}
 
-export default Hello;
+function initThatBus() {
+    console.info('initThatBus');
+    document.body.appendChild(createCanvas());
+}
+
+initThatBus();
