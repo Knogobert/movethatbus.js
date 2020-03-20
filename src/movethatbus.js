ee6f535f4c1248419f64961e899fc381e895e94c
danba340 <danba340@gmail.com>
2020-03-20 22:53:30 +0100

    Support moving the bus

--- src/movethatbus.js
+++ src/movethatbus.js
@@ -4,62 +4,89 @@
  * Use moveThatBus.settings to replace the default settings
  */
 const moveThatBus = {
-    settings: {
-        busSrc: '../src/img/bus.png' // TODO: make sure path is correct
-    },
-    speed: 150,
-    bus: new Image(),
-    _rando(min = 1, max = 10) {
-        return Math.floor(Math.random() * (max - min + 1) + min);
-    },
-    createCanvas() {
-        const canvas = document.createElement('canvas');
+  settings: {
+    busSrc: "../src/img/bus.png", // TODO: make sure path is correct
+    sounds: ["../src/sound/likeabus.mp3", "../src/sound/sound.mp3"]
+  },
+  speed: 150,
+  state: "unmoved",
+  sizeFactor: 1,
+  moveFactor: 1,
+  bus: new Image(),
+  _rando(min = 1, max = 10) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  },
+  createCanvas() {
+    const canvas = document.createElement("canvas");
 
-        // Bus image width
-        canvas.width = 1024;
-        canvas.height = 768;
+    // Bus image width
+    canvas.width = 1024;
+    canvas.height = 768;
 
-        // Base styling
-        canvas.id = "movethatbus";
-        canvas.style.height = "100vh";
-        canvas.style.objectFit = 'cover';
-        canvas.style.position = "fixed";
-        canvas.style.top = "50%";
-        canvas.style.left = "50%";
-        canvas.style.transform = 'translate(-50%, -50%)';
-        canvas.style.overflow = 'hidden';
-        canvas.style.background = "rgba(200,55,77,.2)"; // !refactor
+    // Base styling
+    canvas.id = "movethatbus";
+    canvas.style.height = "100vh";
+    canvas.style.objectFit = "cover";
+    canvas.style.position = "fixed";
+    canvas.style.top = "50%";
+    canvas.style.left = "50%";
+    canvas.style.transform = "translate(-50%, -50%)";
+    canvas.style.overflow = "hidden";
+    canvas.style.background = "rgba(200,55,77,.2)"; // !refactor
 
-        return canvas;
-    },
-    draw(canvas) {
-        const ctx = canvas.getContext('2d');
-        const time = new Date();
-        ctx.globalCompositeOperation = 'destination-over';
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
+    return canvas;
+  },
+  draw(canvas) {
+    const ctx = canvas.getContext("2d");
+    const time = new Date();
+    this.sizeFactor =
+      this.state === "moving" ? this.sizeFactor * 1.18 : this.sizeFactor;
+    this.moveFactor =
+      this.state === "moving" ? this.moveFactor * 1.2 : this.moveFactor;
+    ctx.globalCompositeOperation = "destination-over";
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-        ctx.save();
-        ctx.translate((canvas.width / 2), (canvas.height / 2));
-        ctx.rotate(this._rando(-2,2) * 0.0001 * time.getMilliseconds());
-        ctx.translate(-(canvas.width / 2), -(canvas.height / 2));
-        ctx.drawImage(this.bus, this._rando(0,10), this._rando(0,10), canvas.width, canvas.height);
-        ctx.restore();
+    ctx.save();
+    ctx.translate(canvas.width / 2, canvas.height / 2);
+    ctx.rotate(this._rando(-2, 2) * 0.0001 * time.getMilliseconds());
+    ctx.translate(-(canvas.width / 2), -(canvas.height / 2));
+    ctx.drawImage(
+      this.bus,
+      this._rando(0, 10) - this.moveFactor,
+      this._rando(0, 10),
+      canvas.width + this.sizeFactor,
+      canvas.height + this.sizeFactor
+    );
+    ctx.restore();
 
-        // window.requestAnimationFrame(() => this.draw(canvas));
-    },
-    init() {
-        console.info('initThatBus');
-        this.bus.src = this.settings.busSrc;
-        const canvas = this.createCanvas();
-        document.body.appendChild(canvas);
+    // window.requestAnimationFrame(() => this.draw(canvas));
+  },
+  init() {
+    console.info("initThatBus");
+    this.bus.src = this.settings.busSrc;
+    const canvas = this.createCanvas();
+    document.body.appendChild(canvas);
+    canvas.addEventListener(
+      "click",
+      () => {
+        this.settings.sounds.forEach(sound => new Audio(sound).play());
+        this.state = "moving";
+        setTimeout(() => {
+          canvas.remove();
+          clearInterval(this.drawInterval);
+        }, 7000);
+      },
+      false
+    );
 
-        // Draw bus
-        this.bus.onload = () => {
-            // window.requestAnimationFrame(() => this.draw(canvas));
-            setInterval(() => this.draw(canvas), this.speed);
-        }
-    }
-}
+    // Draw bus
+    this.bus.onload = () => {
+      // window.requestAnimationFrame(() => this.draw(canvas));
+      this.drawInterval = setInterval(() => this.draw(canvas), this.speed);
+    };
+  },
+  destroyCanvas() {}
+};
 
 moveThatBus.init();
 
